a8a470167a8bbda89bf7513938ec7751b4944014
hoakhanhhl <lehoa@gmail.com>
2024-01-29 17:50:28 +0700

    change

--- src/main.cpp
+++ src/main.cpp
@@ -3,15 +3,16 @@
 #include <WiFi.h>
 #include <PubSubClient.h>
 #include <Preferences.h>
-#if true
-  #include <siot_core_lib.h>   // SIOT Core Lib - all packages or you could select each package manually.
-#else
-  #include <WiFiSelfEnroll.h>  // SIOT Core Lib - seft setup wifi network
-#endif
+#include <WebServer.h> 
+// #if true
+//   #include <siot_core_lib.h>   // SIOT Core Lib - all packages or you could select each package manually.
+// #else
+//   #include <WiFiSelfEnroll.h>  // SIOT Core Lib - seft setup wifi network
+// #endif
 
 // Thông tin kết nối WiFi
-// const char* ssid = "P402";
-// const char* password = "88888888";
+const char* ssid = "P402";
+const char* password = "88888888";
 
 // Thông tin kết nối MQTT  Mosquitto
 const char* mqttServer = "192.168.42.105";
@@ -26,35 +27,35 @@ PubSubClient client(espClient);
 MPU9250 mpu;
 
 // TODO ADV: kết nối wifi, su dung lib SOICT CORE LIB
-WiFiSelfEnroll * MyWiFi = new WiFiSelfEnroll();
+// WiFiSelfEnroll * MyWiFi = new WiFiSelfEnroll();
 
 // Đối tượng Preferences
 Preferences preferences;
 
 // Kết nối WiFi
-// void setup_wifi() {
-//   Serial.print("Connecting to ");
-//   Serial.println(ssid);
-//   WiFi.begin(ssid, password);
-//   while (WiFi.status() != WL_CONNECTED) {
-//     delay(500);
-//     Serial.print(".");
-//   }
-//   Serial.println("");
-//   Serial.println("WiFi connected");
-//   Serial.println("IP address: ");
-//   Serial.println(WiFi.localIP());
-// }
 void setup_wifi() {
-  //just for debug
-  Serial.begin(115200);
-  // Make sure WiFi ssid/password is correct. Otherwise, raise the Adhoc AP Station with ssid = SOICT_CORE_BOARD and password =  12345678
-  MyWiFi->setup();
-  // Release the memory allocated for WiFi Station Handler after finishing his work
-  delete  MyWiFi;
-  MyWiFi = _NULL;
-  // TODO something
+  Serial.print("Connecting to ");
+  Serial.println(ssid);
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("");
+  Serial.println("WiFi connected");
+  Serial.println("IP address: ");
+  Serial.println(WiFi.localIP());
 }
+// void setup_wifi() {
+//   //just for debug
+//   Serial.begin(115200);
+//   // Make sure WiFi ssid/password is correct. Otherwise, raise the Adhoc AP Station with ssid = SOICT_CORE_BOARD and password =  12345678
+//   MyWiFi->setup();
+//   // Release the memory allocated for WiFi Station Handler after finishing his work
+//   delete  MyWiFi;
+//   MyWiFi = _NULL;
+//   // TODO something
+// }
 
 void setup() {
   Serial.begin(115200);
@@ -88,9 +89,9 @@ void setup() {
 }
 
 void loop() {
-  Serial.println(MyWiFi->GetDeviceID());
-  Serial.printf("  %s / %s \n", MyWiFi->GetSSID(), MyWiFi->GetPassword());
-  delay(1000);
+  // Serial.println(MyWiFi->GetDeviceID());
+  // Serial.printf("  %s / %s \n", MyWiFi->GetSSID(), MyWiFi->GetPassword());
+  // delay(1000);
   // Đọc dữ liệu từ cảm biến
   mpu.update();
   // Lấy giá trị gia tốc, góc quay và cảm biến từ trường theo x,y,z
@@ -107,13 +108,13 @@ void loop() {
 
   // Gửi dữ liệu lên MQTT
   String payload = preferences.getString("deviceID", "") + ","
-                   + String(accx / 16384.0) + "," + String(accy / 16384.0) + "," + String(accz / 16384.0) + ","
-                   + String(gyrox / 131.0) + "," + String(gyroy / 131.0) + "," + String(gyroz / 131.0) + ","
+                   + String(accx) + "," + String(accy) + "," + String(accz) + ","
+                   + String(gyrox) + "," + String(gyroy) + "," + String(gyroz) + ","
                    + String(magx) + "," + String(magy) + "," + String(magz);
 
   client.publish(mqttTopic, payload.c_str());
 
   Serial.println("Data sent to MQTT");
   // Đợi 1 giây trước khi gửi dữ liệu tiếp theo
-  delay(1000);
+  delay(5000);
 };
